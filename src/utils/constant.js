5a74fab9a0233092e21f2d37d0f1bf256f23d6be
Ashwini Gupta <ashwinigpt1996@gmail.com>
2022-08-01 12:30:17 +0530

    complete Blog-App

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,3 +1,21 @@
-const BaseUrl = ' https://mighty-oasis-08080.herokuapp.com/api/';
+const ROOT_URL = 'https://mighty-oasis-08080.herokuapp.com/api/';
 
-export default BaseUrl;
+const articleURL = ROOT_URL + 'articles';
+const tagURL = ROOT_URL + 'tags';
+const SignUpURL = ROOT_URL + 'users';
+const loginURL = ROOT_URL + 'users/login';
+const localStorageKey = 'app_user';
+const userVerifyURL = ROOT_URL + 'user';
+const SettingURL = ROOT_URL + 'user';
+
+
+export {
+    ROOT_URL,
+    articleURL,
+    tagURL,
+    SignUpURL,
+    loginURL,
+    localStorageKey,
+    userVerifyURL,
+    SettingURL,
+};
